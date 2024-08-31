28c53cb49d1de4d657fd774885a12b7ed4934ca3
BattaMahendra <bmc26g@gmail.com>
2024-09-01 00:05:14 +0530

    Added kafka learning screenshots.

--- src/main/java/com/kafka/controller/ClientController.java
+++ src/main/java/com/kafka/controller/ClientController.java
@@ -3,12 +3,10 @@ package com.kafka.controller;
 
 import com.kafka.config.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/kafka")
+@RestController
+@RequestMapping("/kafka")
 public class ClientController {
 
     @Autowired
