67049631876393c7fe51944c7ea8e4e7e2444ba1
BATTA_MAHENDRA <battamahendra26@gmail.com>
2025-11-11 15:31:17 +0530

    Added kafka learning screenshots.

--- src/main/java/com/kafka/config/Consumer.java
+++ src/main/java/com/kafka/config/Consumer.java
@@ -9,6 +9,6 @@ public class Consumer {
 
     @KafkaListener(topics = "Mahendra_My_First_Kafka_Topic",groupId = "Mahendra_Topics_Group")
     public void listener(String message){
-        System.out.println("/n/n Received the message: "+message);
+        System.out.println("\n\n Received the message: "+message);
     }
 }
